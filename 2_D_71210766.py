a2be0731f8a3ae31700f667a156d9ba8ddadffe7
71210766 <93719122+71210766@users.noreply.github.com>
2022-11-24 16:03:26 +0700

    Add files via upload

---
+++ 2_D_71210766.py
@@ -0,0 +1,11 @@
+nama1 = input("Masukkan nama pemain pertama : ")
+nama2 = input("Masukkan nama pemain kedua : ")
+nama2 = input("Masukkan nama pemain ketiga : ")
+kartu1 = input("Masukkan jumlah kartu pemain pertama : ")
+kartu2 = input("Masukkan jumlah kartu pemain kedua : ")
+kartu3 = input("Masukkan jumlah kartu pemain ketiga : ")
+
+if
+
+
+
